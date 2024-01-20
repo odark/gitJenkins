0cfa57363076a97762984501f94b25e5463eb1b7
odark <odark@naver.com>
2024-01-20 18:57:06 +0900

    hellojs update2

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ const http = require('node:http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({
-    data: 'Hello World!!!!!!',
+    data: 'Hello World2',
   }));
 });
 
